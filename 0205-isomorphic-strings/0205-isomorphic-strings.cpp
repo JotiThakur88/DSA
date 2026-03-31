3f03e1a15420cedd4f77cf6dbcf17c213be17c26
Jyoti <151373033+JotiThakur88@users.noreply.github.com>
2026-03-31 10:29:06 +0530

    Time: 3 ms (39.67%), Space: 9.5 MB (6.98%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,16 @@
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> m1, m2;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
